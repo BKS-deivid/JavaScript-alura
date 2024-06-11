ffe691d04fd692fae46ea53de0cf6033ab282c7e
BKS-deivid <163128890+BKS-deivid@users.noreply.github.com>
2024-06-11 09:52:56 -0300

    Update and rename README.md to 7conversoes.js

---
+++ 7conversoes.js
@@ -0,0 +1,22 @@
+// Tipo Dado
+// booleanos
+
+// Conversao Implicita
+const numero = "456";
+const numeroString = "456";
+
+console.log(numero === numeroString);
+
+// Conversaoimplicita
+// Acontece quando o Javascript convere o numero em string e depois
+// as 2 strings
+console.log(numero == numeroString);
+console.log(numero + numeroString);
+
+// Conversao Explicita
+// Number
+let novoNumero = Number(neumeroString);
+console.log(numero + novoNumero);
+
+// String
+
